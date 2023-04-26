b676d828db83f1b7aaf12c67c85793707c672d89
mangesh <mbchate08@gmail.com>
2023-04-26 23:11:07 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 
 
 export default function Header() {
 
-    const logo = `https://www.pngkit.com/png/detail/78-788100_fire-logo-png-svg-free-download-fire-logo.png`
+
+
+
+  const logo = `https://www.pngkit.com/png/detail/78-788100_fire-logo-png-svg-free-download-fire-logo.png`
 
 
   return (
-   <div>
-      <nav className="navbar bg-body-tertiary">
+    <div>
+      <nav className="navbar bg-body-tertiary navbar-dark">
         <div className="container-fluid">
           <a className="navbar-brand d-flex align-items-center" href="/">
-            <img src={logo} alt="Logo" width="90"  className=" rounded-5"/>
-            <h2 className='fw-bolder text-info-emphasis mt-3'> Unicorn Code</h2>
+            <img src={logo} alt="Logo" width="90" className=" rounded-5" />
+            <h2 className='fw-bolder text-info-emphasis mt-3'> Unicorn Code Editor</h2>
           </a>
-          
+
         </div>
+
       </nav>
-   </div>
+    </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,20 @@
-import React, { useState , useRef, useEffect} from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import AceEditor from "react-ace";
+
+
+import 'ace-builds/src-noconflict/theme-twilight';
+import 'ace-builds/src-noconflict/mode-html';
+
+import 'ace-builds/src-noconflict/mode-css';
+import 'ace-builds/src-noconflict/mode-javascript';
+
+
+
 
 export default function Home() {
 
-  
+
+
   const [html, setHtml] = useState('');
   const [css, setCss] = useState('');
   const [js, setJs] = useState('');
@@ -12,81 +24,154 @@ export default function Home() {
   useEffect(() => {
     const outputDoc = iframeRef.current.contentDocument;
     outputDoc.body.innerHTML = html + '<style>' + css + '</style>';
-    outputDoc.defaultView.eval(js);
+    const script = document.createElement('script');
+    script.textContent = js;
+    outputDoc.body.appendChild(script);
   }, [html, css, js]);
-
-
   
+
+
+
+
+
+
+
   return (
     <div className='container-fluid mt-4 ' >
-    <div className='justify-content-end d-flex'>
-    
+      <div className='justify-content-end d-flex'>
 
 
-    </div>
-    
+
+      </div>
+
       <div className="row d-flex justify-content-center flex-wrap">
         <div className="col-lg-4 col-md-12 col-sm-12 mt-3">
-          <div className=" bg-light text-info-emphasis rounded-4 shadow" style={{ height: "30rem", border: "2px solid grey" }}>
-            <div className="container-fluid bg-dark text-light rounded-top-3 p-2 ">
+          <div className=" bg-light text-info-emphasis  shadow" style={{ height: "30rem", border: "2px solid grey" }}>
+            <div className="container-fluid bg-dark text-light  p-2 ">
 
               <span className='fw-bold font-monospace'>HTML</span>
 
 
             </div>
-            <div className='container-fluid'>
-              <textarea rows="17" className='form-text form-control bg-light container-fluid border-0 rounded-0 outline-none w-100 ' onChange={(e)=>{setHtml(e.target.value)}} ></textarea>
 
-            </div>
+
+            <AceEditor
+
+              mode="html"
+              theme="twilight"
+              name="my-editor"
+              onChange={(value) => setHtml(value)}
+              fontSize={18}
+              editorProps={{ $blockScrolling: true }}
+              width='100%'
+              height='434px'
+             
+
+            />
+
+            <style jsx>{`
+      
+        .ace_editor {
+          font-family:"monospace" ;
+          
+        }
+        
+      
+      
+      
+      `}</style>
+
           </div>
         </div>
-        <div className="col-lg-4 col-md-12 col-sm-12 mt-3">
-          <div className=" bg-light text-info-emphasis rounded-4 shadow" style={{ height: "30rem", border: "2px solid grey" }}>
-            <div className="container-fluid bg-dark text-light rounded-top-3 p-2 ">
+        <div className="col-lg-4 col-md-12 col-sm-12 mt-3 ">
+          <div className=" bg-light text-info-emphasis  shadow" style={{ height: "30rem", border: "2px solid grey" }}>
+            <div className="container-fluid bg-dark text-light  p-2 ">
 
               <span className='fw-bold font-monospace'>CSS</span>
 
 
             </div>
-            <div className='container-fluid'>
-              <textarea rows="17" className='form-text form-control bg-light container-fluid border-0 rounded-0 outline-none w-100' onChange={(e)=>{setCss(e.target.value)}}></textarea>
+            <AceEditor
 
-            </div>
+              mode="css"
+              theme="twilight"
+              name="my-editor"
+              onChange={(value) => setCss(value)}
+              fontSize={18}
+              editorProps={{ $blockScrolling: true }}
+              width='100%'
+              height='434px'
+        
+
+            />
+
+            <style jsx>{`
+
+.ace_editor {
+font-family:"monospace" ;
+
+}
+
+
+
+
+`}</style>
           </div>
         </div>
         <div className="col-lg-4 col-md-12 col-sm-12 mt-3">
-          <div className=" bg-light text-info-emphasis rounded-4 shadow" style={{ height: "30rem", border: "2px solid grey" }}>
-            <div className="container-fluid bg-dark text-light rounded-top-3 p-2 ">
+          <div className=" bg-light text-info-emphasis  shadow" style={{ height: "30rem", border: "2px solid grey" }}>
+            <div className="container-fluid bg-dark text-light  p-2 ">
 
               <span className='fw-bold font-monospace'>JAVASCRIPT</span>
 
 
             </div>
-            <div className='container-fluid'>
-              <textarea rows="17" className='form-text form-control bg-light container-fluid border-0 rounded-0 outline-none w-100' onChange={(e)=>{setJs(e.target.value)}}></textarea>
+            <AceEditor
 
-            </div>
+              mode="javascript"
+              theme="twilight"
+              name="my-editor"
+              onChange={(value) => setJs(value)}
+              fontSize={18}
+              editorProps={{ $blockScrolling: true }}
+              width='100%'
+              height='434px'
+             
+
+            />
+
+            <style jsx>{`
+
+.ace_editor {
+font-family:"monospace" ;
+
+}
+
+
+
+
+`}</style>
           </div>
         </div>
 
       </div>
 
 
-     
-        <div className="col-lg-12 col-md-12  col-sm-12 bg-light text-info-emphasis rounded-4 mt-4 mb-5 shadow" style={{ height: "30rem", border: "2px solid grey" }}>
-          <div className="container-fluid bg-dark text-light rounded-top-3 p-2 ">
 
-            <span className='fw-bold font-monospace'>OUTPUT</span>
+      <div className="col-lg-12 col-md-12  col-sm-12 bg-light text-info-emphasis rounded-4 mt-4 mb-5 shadow" style={{ height: "30rem", border: "2px solid grey" }}>
+        <div className="container-fluid bg-dark text-light rounded-top-3 p-2 ">
+
+          <span className='fw-bold font-monospace'>OUTPUT</span>
 
 
-          </div>
-         
-          <iframe className='container-fluid' style={{height:"26rem"}} id="output" ref={iframeRef}></iframe>
-
-        
         </div>
 
-    
+        <iframe className='container-fluid' style={{ height: "26rem" }} id="output" ref={iframeRef}></iframe>
+
+
+      </div>
+
+
 
     </div>
   )
